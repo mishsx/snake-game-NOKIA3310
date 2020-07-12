b6a1be14711a1212b483b25da9fabb212a2d519a
Shubham Mishra <mishrashubhamx@gmail.com>
2020-07-12 11:43:21 -0400

    Added control key logic to handle snake's movement.

--- app.js
+++ app.js
@@ -8,6 +8,27 @@ document.addEventListener('DOMContentLoaded', () =>{
   let appleIndex = 0; // first div in our grid
   let currentSnake = [2,1,0]; // 2 denotes HEAD of snake, 1 is the body part, and 0 is the TAIL
 
-  
+  let direction =1;
+  let score =0;
+  let speed = 0.9;
+  let intervalTime = 0;
+  let interval = 0;
+
+  //Movement of snake on board using keycodes.
+
+  function control(e){
+    squares[currentIndex].classList.remove('snake'); // Removing snakes class from all the squares
+
+    if(e.keyCode === 39){
+      direction = 1;
+    }else if(e.keycode === 38){
+      direction = -width;
+    }else if(e.keyCode === 37){
+      direction = -1;
+    }else if(e.keyCode === 40){
+      direction = +width;
+    }
+  }
+  document.addEventListener('keyup', control);
 
 });
