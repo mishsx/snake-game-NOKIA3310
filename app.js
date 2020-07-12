98c95bc1660cda32e7a601d9c84126fbb99ec1c1
Shubham Mishra <mishrashubhamx@gmail.com>
2020-07-12 09:22:48 -0400

    Added styling of gameboard, added declarations for our game in JS

---
+++ app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () =>{
+  const squares= document.querySelectorAll('.grid div');
+  const scoreDisplay= document.querySelector('span');
+  const startBtn = document.querySelector('.start');
+
+  const width=10;
+  let currentIndex = 0; //first div in our grid
+  let appleIndex = 0; // first div in our grid
+  let currentSnake = [2,1,0]; // 2 denotes HEAD of snake, 1 is the body part, and 0 is the TAIL
+
+  
+
+});
